0216bcee7333e20cfe780eaecc154b9e41b1a472
José Rogério <roger.sama@gmail.com>
2026-04-27 19:24:01 +0200

    chore(config): config static export website

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 export const baseUrl = "https://portfolio-blog-starter.vercel.app";
 
+export const dynamic = 'force-static'
+
 export default async function sitemap() {
   let routes = [""].map((route) => ({
     url: `${baseUrl}${route}`,
