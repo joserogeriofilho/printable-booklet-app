a47f1d992fd55af5c3b3a94489b6da7f08954cad
José Rogério <roger.sama@gmail.com>
2026-05-01 02:31:46 +0200

    feat(component): puts the theme toggle to the end of the nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -23,7 +23,7 @@ export function Navbar() {
     <aside className="-ml-[8px] mb-8 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-center relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
           <div className="flex flex-row items-center space-x-0 pr-10">
@@ -43,6 +43,8 @@ export function Navbar() {
                 </Link>
               );
             })}
+          </div>
+          <div className="ml-auto">
             <ThemeToggle />
           </div>
         </nav>
