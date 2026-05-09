6ffa48bc3c5802a8b44348974faca942a94012d0
José Rogério <roger.sama@gmail.com>
2026-05-09 15:52:38 +0200

    feat(pages): creates buttons for linkedin and github profile (todo)

--- app/[locale]/about-me/page.tsx
+++ app/[locale]/about-me/page.tsx
@@ -14,6 +14,24 @@ export default function Page() {
         <p className="text-sm text-stone-500 dark:text-stone-400 leading-relaxed max-w-md">
           {t("description")}
         </p>
+
+        <a
+          href="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block mt-4 px-4 py-2 bg-stone-800 text-stone-100 hover:bg-stone-600 dark:bg-stone-600 dark:text-stone-100 dark:hover:bg-stone-400 transition-colors duration-200 rounded mr-4"
+        >
+          Github
+        </a>
+
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block mt-4 px-4 py-2 bg-blue-600 text-stone-100 hover:bg-blue-700 dark:bg-blue-600 dark:text-stone-100 dark:hover:bg-blue-400 transition-colors duration-200 rounded"
+        >
+          Linkedin
+        </a>
       </header>
     </section>
   );
