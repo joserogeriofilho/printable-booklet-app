c113cd3ad5ad15ca83ba63e53a71dd5c6958cb99
José Rogério <roger.sama@gmail.com>
2026-05-28 00:55:06 +0200

    refactor: removes unecessary buymeacoffee script

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Script from "next/script";
 
 const supportedLocales = ["en", "pt", "es"];
 
@@ -20,19 +19,5 @@ export default function RootPage() {
     router.replace(`/${locale}/`);
   }, [router]);
 
-  return (
-    <Script
-      type="text/javascript"
-      src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
-      data-name="bmc-button"
-      data-slug="roger.sama"
-      data-color="#FFDD00"
-      data-emoji=""
-      data-font="Cookie"
-      data-text="Buy me a coffee"
-      data-outline-color="#000000"
-      data-font-color="#000000"
-      data-coffee-color="#ffffff"
-    ></Script>
-  );
+  return null;
 }
